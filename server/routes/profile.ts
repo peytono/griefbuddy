825142e277d26528b7bc85df53aa6b2570ce8a76
peytono <pcopcoeco@gmail.com>
2024-07-19 23:54:44 -0500

    create new patch for profile

--- server/routes/profile.ts
+++ server/routes/profile.ts
@@ -2,12 +2,13 @@
 // import { Request, Response } from 'express';
 import express, { Request, Response } from 'express';
 
-const router = express.Router();
+const profile = express.Router();
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
+const { User } = prisma;
 
-router.get('/user', (req: Request, res: Response) => {
+profile.get('/user', (req: Request, res: Response) => {
   prisma.User.findMany(req.body)
     .then((results: any) => {
       res.status(200).send(results);
@@ -18,21 +19,33 @@ router.get('/user', (req: Request, res: Response) => {
     });
 });
 
-router.patch('/user', (req: Request, res: Response) => {
-  // const { googleId, location, currMood, friendName, friendNumber, friendRelationship, nickname }
-  // = req.body
-//   console.log('/user req.body', req.body);
-  // changed to update from updateMany - now sends the updated profile response
-  //   prisma.User.updateMany(req.body)
-  prisma.User.update(req.body)
-    .then((response: Response) => {
-    //   console.log('patch /user prisma update response', response);
-      res.status(200).send(response);
-    })
-    .catch((err: string) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
+profile.patch('/', async (req: Request, res: Response) => {
+  const { id, data } = req.body;
+  try {
+    const updatedUser = await User.update({ where: { id }, data });
+    console.log(updatedUser);
+    res.send(updatedUser);
+  } catch (err) {
+    console.error('failed updating user: ', err);
+    res.sendStatus(500);
+  }
 });
+// router.patch('/user', (req: Request, res: Response) => {
+//   // const
+// { googleId, location, currMood, friendName, friendNumber, friendRelationship, nickname }
+//   // = req.body
+// //   console.log('/user req.body', req.body);
+//   // changed to update from updateMany - now sends the updated profile response
+//   //   prisma.User.updateMany(req.body)
+//   prisma.User.update(req.body)
+//     .then((response: Response) => {
+//     //   console.log('patch /user prisma update response', response);
+//       res.status(200).send(response);
+//     })
+//     .catch((err: string) => {
+//       console.error(err);
+//       res.sendStatus(500);
+//     });
+// });
 
-export = router;
+export = profile;
